cedc99518ca6f83dd285d3f2d618439bc97ac54d
bmbrost <bmbrost@gmail.com>
2015-08-11 18:31:36 -0600

    Add code to estimate status at unobserved times

--- probit.semipar.sim.R
+++ probit.semipar.sim.R
@@ -12,8 +12,12 @@ time <- c(0,cumsum(rgamma(T-1,shape=1.1,scale=4.5)))  # time covariate
 hr <- ((time)-24*floor(time/24))
 day <- ceiling(time/24)
 X <- cbind(1,day,hr)
-X[,-1] <- scale(X[,-1])
 qX <- ncol(X)
+
+# Center and scale design matrix
+X.mean <- apply(X,2,mean)
+X.sd <- apply(X,2,sd)
+X[,-1] <- apply(X[,-1],2,function(x) (x-mean(x))/sd(x))
 
 # beta <- c(-0.1,1.25,0.5)  # Coefficients on X
 beta <- c(-0.5,1.25,0.5)  # Coefficients on X
@@ -29,7 +33,7 @@ knots <- seq(0,max(time),by=int)
 Z <- bs(time,knots=knots,degree=3,intercept=FALSE)  # cubic spline
 qZ <- ncol(Z)
 
-sigma.alpha <- 5
+sigma.alpha <- 20
 alpha <- rnorm(qZ,0,sigma.alpha)
 trend <- Z%*%alpha
 
@@ -92,7 +96,7 @@ abline(h=0,col=2,lty=2)
 
 # Define non-linear trend to model non-parametrically 
 trend <- 0.5*sin(0.1*time)  # non-linear pattern
-trend <- 2*sin(0.1*time)  # non-linear pattern
+# trend <- 2*sin(0.1*time)  # non-linear pattern
 plot(time,trend,type="l")
 
 # Simulate data
@@ -152,6 +156,114 @@ plot(time,trend,type="l")
 plot(alpha.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(alpha.quant)))
 lines(alpha.hat,col=rgb(0,0,0,0.25))
 abline(h=0,col=2,lty=2)
+
+
+#########################################################
+### Estimate status at unobserved times
+#########################################################
+
+T <- 100  # number of observations
+T.tilde <- 100  # number of unobserved times at which to predict status
+
+# Define covariates
+time <- c(0,cumsum(rgamma((T+T.tilde)-1,shape=1.1,scale=20)))  # time covariate
+hist(time[-1]-time[-(T+T.tilde)])
+hr <- ((time)-24*floor(time/24))
+day <- ceiling(time/24)
+X <- cbind(1,day,hr)
+qX <- ncol(X)
+
+# Center and scale design matrix
+X.mean <- apply(X,2,mean)
+X.sd <- apply(X,2,sd)
+X[,-1] <- apply(X[,-1],2,function(x) (x-mean(x))/sd(x))
+
+# beta <- c(-0.1,1.25,0.5)  # Coefficients on X
+beta <- c(-0.5,1.25,0.5)  # Coefficients on X
+beta <- c(0.0,0.0,0.0)  # Coefficients on X
+
+# Define non-linear trend to model non-parametrically 
+trend <- 0.5*sin(0.1*time)  # non-linear pattern
+trend <- 20*sin(0.01*time)  # non-linear pattern
+plot(time,trend,type="l")
+
+# Simulate data
+p <- pnorm(X%*%beta+trend)  # probability of being hauled-out
+hist(p);summary(p)
+y <- rbinom(T+T.tilde,1,p)  # haulout indicator variable: 1=hauled-out, 0=at-sea
+table(y)
+
+plot(time,y,ylim=range(c(trend,y,X%*%beta)))
+lines(time,X%*%beta,col=2)
+lines(time,trend,col=3)
+lines(time,X%*%beta+trend,col=4)
+
+# B-spline basis expansion
+int <- 100  # interval between knots
+knots <- seq(0,max(time),by=int)
+Z <- bs(time,knots=knots,degree=3,intercept=FALSE)  # cubic spline
+matplot(Z,type="l")
+qZ <- ncol(Z)
+
+source('~/Documents/git/SemiparametricRegression/probit.semipar.mcmc.R', chdir = TRUE)
+start <- list(beta=beta,alpha=rep(0,qZ))
+# hist(sqrt(1/rgamma(1000,1,,2)))
+priors <- list(mu.beta=rep(0,qX),sigma.beta=10)
+idx <- sort(sample(1:(T+T.tilde),T.tilde))  # subset for model fitting
+out1 <- probit.semipar.mcmc(y[idx],X[idx,],Z[idx,],
+	priors=priors,start=start,sigma.alpha=10,n.mcmc=1000)
+out1$DIC
+
+matplot(out1$beta,type="l",lty=1);abline(h=beta,col=1:3,lty=2)
+
+beta.hat <- apply(out1$beta,2,mean)
+beta.quant <- t(apply(out1$beta,2,quantile,c(0.025,0.975)))
+plot(beta.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(beta.quant)))
+abline(h=0,col=2,lty=2)
+segments(1:qX,beta.quant[,1],1:qX,beta.quant[,2],col="lightgrey")
+points(beta.hat,pch=19,col=rgb(0,0,0,0.25))
+points(beta,pch=19)
+
+alpha.hat <- apply(out1$alpha,2,mean)
+alpha.quant <- t(apply(out1$alpha,2,quantile,c(0.025,0.975)))
+plot(alpha.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(alpha.quant)))
+abline(h=0,col=2,lty=2)
+segments(1:qZ,alpha.quant[,1],1:qZ,alpha.quant[,2],col="lightgrey")
+points(alpha.hat,pch=19,col=rgb(0,0,0,0.25))
+boxplot(pnorm(out1$u),col=8,outline=FALSE)
+points(y[idx],col=3,pch=19,cex=0.5)
+
+u.inv <- matrix(pnorm(out1$u),,T)
+u.inv.mean <- apply(u.inv,2,mean)
+u.inv.quant <- t(apply(u.inv,2,quantile,c(0.025,0.975)))
+plot(u.inv.mean,pch=19,col=rgb(0,0,0,0.25),ylim=c(0,1))
+segments(1:T,u.inv.quant[,1],1:T,u.inv.quant[,2],col=rgb(0,0,0,0.15))
+points(y[idx],col=3,pch=19,cex=0.5)
+
+par(mfrow=c(2,1))
+plot(time,trend,type="l")
+plot(alpha.hat,pch=19,col=rgb(0,0,0,0.25),ylim=c(range(alpha.quant)))
+lines(alpha.hat,col=rgb(0,0,0,0.25))
+abline(h=0,col=2,lty=2)
+
+# Prediction for unobserved times 
+u.tilde <- apply(out1$beta,1,function(x) X[-idx,]%*%x)+
+	apply(out1$alpha,1,function(x) Z[-idx,]%*%x)
+u.tilde.inv <- matrix(pnorm(u.tilde),,T,byrow=TRUE)
+u.tilde.inv.mean <- apply(u.tilde.inv,2,mean)
+u.tilde.inv.quant <- t(apply(u.tilde.inv,2,quantile,c(0.025,0.975)))
+plot(u.tilde.inv.mean,pch=19,col=rgb(0,0,0,0.25),ylim=c(0,1))
+segments(1:T,u.tilde.inv.quant[,1],1:T,u.tilde.inv.quant[,2],col=rgb(0,0,0,0.15))
+abline(h=0.5,col=2,lty=2)
+points(y[-idx],col=3,pch=19,cex=0.5)
+
+# Examine fit relative to distance to nearest observation
+t.dist <- sapply(time[-idx],function(x) min(abs(x-time[idx])))
+plot(t.dist)
+plot(abs(y[-idx]-u.tilde.inv.mean))
+plot(t.dist,abs(y[-idx]-u.tilde.inv.mean))
+plot(t.dist,u.tilde.inv.quant[,2]-u.tilde.inv.quant[,1])
+
 
 
 ###
