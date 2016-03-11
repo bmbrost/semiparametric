8c6fae983d1b85294da4e47141ab3f97ba0e86d9
bmbrost <bmbrost@gmail.com>
2016-03-11 10:13:40 -0700

    Create semi parametric mixed model

---
+++ probit.semipar.mixed.mcmc.R
@@ -0,0 +1,211 @@
+probit.semipar.mixed.mcmc <- function(y,X,g,W,priors,start,n.mcmc){
+
+	###
+	### Brian M. Brost (09 MAR 2016)
+	### Mixed effects regression for binary data using probit link
+	###
+
+	###
+	### Model statement:
+	### y_j(t)=0,v_j(t)<=0
+	### y_j(t)=1,v_j(t)>0
+	### v_ij ~ N(x_j(t)*beta_j+w_j(t)*alpha_j,1)
+	### beta_j ~ N(mu.beta,Lambda)
+	###	mu.beta ~ N(0,sigma.beta^2*I)
+	### alpha ~ N(0,sigma.alpha.j^2*I)
+	###	Lambda ~ Wish(S0,nu)
+	### sigma.alpha.j ~ IG(r,q)
+	###	
+	
+	###
+	###  Libraries and Subroutines
+	###
+	
+	truncnormsamp <- function(mu,sig2,low,high,nsamp){
+	  flow=pnorm(low,mu,sqrt(sig2)) 
+	  fhigh=pnorm(high,mu,sqrt(sig2)) 
+	  u=runif(nsamp) 
+	  tmp=flow+u*(fhigh-flow)
+	  x=qnorm(tmp,mu,sqrt(sig2))
+	  x
+	}
+	
+	###
+	###  Preliminary Variables
+	###
+
+# browser()
+	J <- length(unique(g))  # number of groups
+	g.idx <- sapply(sort(unique(g)),function(x) which(g==x),simplify=FALSE)
+		# indexes of observations in y by group
+	n.j <- unlist(lapply(g.idx,length))  # number of observations per group
+	n <- length(y)  # total number of observations
+	qX <- ncol(X)
+	qW <- unlist(lapply(W,ncol))
+	y1 <- (y==1)
+	y0 <- (y==0)
+	y1.sum <- sum(y1)
+	y0.sum <- sum(y0)
+	v <- numeric(n)
+
+	###
+	### Starting values and priors
+ 	###
+
+# browser()
+	beta <- matrix(start$beta,qX,J)
+# alpha <- start$alpha
+	mu.beta <- matrix(start$mu.beta,qX,1)
+	Lambda <- start$Lambda
+	Lambda.inv <- solve(Lambda)
+xi <- rep(1,qX)
+	Sigma.alpha <- lapply(qW,function(x) diag(x)*start$sigma.alpha^2)
+	Sigma.alpha.inv <- lapply(Sigma.alpha,solve)
+	W.cross <- lapply(W,crossprod)  # t(W)%*%Wcross product of W
+
+	Sigma.beta <- priors$sigma.beta^2*diag(qX)
+	Sigma.beta.inv <- solve(Sigma.beta)
+	S0 <- priors$S0
+	nu <- priors$nu
+	r <- priors$r
+	q <- priors$q
+
+	###
+	### Create receptacles for output
+	###
+  
+	beta.save <- array(0,dim=c(n.mcmc,qX,J))
+	mu.beta.save <- matrix(0,n.mcmc,qX)
+	xi.save <- matrix(0,n.mcmc,qX)
+	alpha.save <- lapply(qW,function(x) matrix(0,n.mcmc,x))
+	Lambda.save <- array(0,dim=c(qX,qX,n.mcmc))
+	sigma.alpha.save <- matrix(0,n.mcmc,J)
+	v.save <- matrix(0,n.mcmc,n)
+
+	keep <- list(xi=0)
+	
+	###
+	###  Gibbs Loop
+	###
+	
+	for(k in 1:n.mcmc){
+		if(k%%1000==0) cat(k," ");flush.console()
+
+		###
+		### Update alpha.j
+		###
+
+		for(i in 1:J){
+			idx <- g.idx[[i]]
+			b <- crossprod(W[[i]],(v[idx]-X[idx,]%*%beta[,i]))
+			A.inv <- solve(W.cross[[i]]+Sigma.alpha.inv[[i]])
+			alpha[[i]] <- A.inv%*%b+t(chol(A.inv))%*%matrix(rnorm(qW[i]),qW[i],1)
+			# alpha[[i]] <- t(rmvnorm(1,A.inv%*%b,A.inv))
+			alpha.save[[i]][k,] <- alpha[[i]]
+		}
+
+	  	###
+	  	### Sample beta_j
+	  	###
+# browser()		
+		for(i in 1:J){
+			idx <- g.idx[[i]]
+			b <- crossprod(X[idx,],(v[idx]-W[[i]]%*%alpha[[i]])) + mu.beta
+			A.inv <- solve(t(X[idx,])%*%X[idx,]+Lambda.inv) 
+			beta[,i] <- A.inv%*%b+t(chol(A.inv))%*%matrix(rnorm(qX),qX,1)
+			# beta[,i] <- t(rmvnorm(1,A.inv%*%b,A.inv))
+		}
+
+		###
+		### Sample v (auxilliary variable for probit regression)
+	  	###
+
+		linpred <- rowSums(X*t(beta[,g]))+c(sapply(1:J,function(x) W[[x]]%*%alpha[[x]]))
+		v[y1] <- truncnormsamp(linpred[y1],1,0,Inf,y1.sum)
+	  	v[y0] <- truncnormsamp(linpred[y0],1,-Inf,0,y0.sum)
+
+		###
+		### Update sigma.alpha.j
+		###
+
+		r.tmp <- 1/(unlist(lapply(alpha,function(x) sum(x^2)))/2+1/r)
+		q.tmp <- qW/2+q
+		sigma2.alpha <- 1/sapply(r.tmp,function(x) rgamma(1,q.tmp,,x))
+		for(i in 1:J) diag(Sigma.alpha[[i]]) <- sigma2.alpha[i]
+		Sigma.alpha.inv <- lapply(Sigma.alpha,solve)
+# sigma2.alpha <- 1
+			
+	  	###
+	  	### Sample mu_beta
+	  	###
+
+		beta.mean <- apply(beta,1,sum)
+		A.inv <- solve(J*Lambda.inv+Sigma.beta.inv)
+		b <- Lambda.inv%*%beta.mean
+	    mu.beta <- A.inv%*%b+t(chol(A.inv))%*%matrix(rnorm(qX),qX,1)
+		# mu.beta <- t(rmvnorm(1,A.inv%*%b,A.inv))
+	  	
+	  	###
+	  	### Sample Lambda
+	  	###
+		
+	  	Sn <- S0+crossprod(t(beta)-matrix(mu.beta,J,2,byrow=TRUE))
+		Q <- solve(rWishart(1,nu+J,solve(Sn))[,,1])
+		
+		Lambda <- solve(rWishart(1,nu+J,solve(Sn))[,,1])
+		# Lambda.inv <- solve(Lambda)
+		
+		###
+		### Sample xi (scaling factors)
+		###
+# browser()	
+		xi.star <- rnorm(qX,xi,0.025)
+		idx <- which(xi>0)
+		for(i in idx){
+# i <- 2
+			xi.tmp <- xi
+			xi.tmp[i] <- xi.star[i]
+			Lambda.star <- (diag(qX)*xi.tmp)%*%Q%*%(diag(qX)*xi.tmp)
+
+			# Q[1,1]*xi.star[1]^2
+			# Q[1,2]*xi.star[1]*xi[2]
+			# Q[2,2]*xi.star[2]^2
+			# Q[1,2]*xi[1]*xi.star[2]
+# browser()			
+
+			mh.star.xi <- sum(dmvnorm(t(beta),mu.beta,Lambda.star,log=TRUE))
+			mh.0.xi <- sum(dmvnorm(t(beta),mu.beta,Lambda,log=TRUE))
+
+			if(exp(mh.star.xi-mh.0.xi)>runif(1)){
+	        	xi[i] <- xi.star[i]
+				Lambda <- Lambda.star
+				keep$xi <- keep$xi+1
+				# keep.tmp$sigma.mu <- keep.tmp$sigma.mu+1
+	    	} 
+		}		
+
+		Lambda.inv <- solve(Lambda)
+				
+	  	###
+	  	### Save Samples 
+	  	###
+
+	  	beta.save[k,,] <- beta
+	  	v.save[k,] <- v
+		mu.beta.save[k,] <- mu.beta
+		sigma.alpha.save[k,] <- sqrt(sigma2.alpha)
+		Lambda.save[,,k] <- Lambda
+		xi.save[k,] <- xi
+	}
+	
+	keep$xi <- keep$xi/(qX*n.mcmc)
+	print(keep$xi)
+	
+	###
+	###  Write output 
+	###
+	
+	list(beta=beta.save,v=v.save,mu.beta=mu.beta.save,Lambda=Lambda.save,
+		alpha=alpha.save,sigma.alpha=sigma.alpha.save,xi=xi.save,keep=keep,
+		y=y,X=X,g=g,W=W,start=start,priors=priors,n.mcmc=n.mcmc)
+}
---
+++ probit.semipar.mixed.sim.R
@@ -0,0 +1,96 @@
+###
+### Simulate binary data and fit model using probit.mixed.mcmc.R
+###
+
+rm(list=ls())
+library(mvtnorm)
+
+T <- 100  # number of observations per group
+J <- 10  # number of groups
+g <- rep(1:J,each=T)  # grouping variable
+
+# Define covariates
+time <- c(replicate(J,c(rgamma(1,shape=10.1,scale=4.5),
+	cumsum(rgamma(T-1,shape=1.1,scale=4.5)))))  # time covariate
+X <- cbind(1,time)
+qX <- ncol(X)
+X[,2] <- scale(X[,2])
+
+mu.beta <- matrix(c(-1,1.25),,1)  # mean of betas
+rho <- 0.5  # correlation between betas
+Lambda <- diag(qX)*1  # variance-covariance of betas
+Lambda[1,2] <- Lambda[2,1] <- Lambda[1,1]*Lambda[2,2]*rho
+
+beta <- t(rmvnorm(J,mu.beta,Lambda))  # betas for each group
+plot(t(beta))
+
+
+# Basis expansion
+int <- 50  # interval between knots
+tapply(time,g,range)
+
+# List of basis exansions, 1 per group
+W <- sapply(1:J,function(x) bs(time[g==x],degree=3,intercept=FALSE,
+	knots=seq(min(time[g==x]),max(time[g==x]),by=int)),simplify=FALSE)
+qW <- unlist(lapply(W,ncol))
+
+sigma.alpha <- 1
+alpha <- lapply(qW,function(x) t(rmvnorm(1,rep(0,x),sigma.alpha^2*diag(x))))
+trend <- sapply(1:J,function(x) W[[x]]%*%alpha[[x]])
+matplot(trend,type="l",lty=1)
+trend <- c(trend)
+
+# Simulate data
+beta.tmp <- t(beta[,g])
+p <- pnorm(rowSums(X*beta.tmp)+trend)
+hist(p);summary(p)
+
+y <- rbinom(T*J,1,p)
+table(y)
+plot(p,y)
+
+plot(time,y,ylim=range(c(trend,y,rowSums(X*beta.tmp))))
+matplot(matrix(time,T,J),matrix(rowSums(X*beta.tmp),T,J),col=2,type="l",lty=2,add=TRUE)
+matplot(matrix(time,T,J),matrix(trend,T,J),col=3,type="l",lty=2,add=TRUE)
+matplot(matrix(time,T,J),matrix(rowSums(X*beta.tmp)+trend,T,J),
+	col=4,type="l",lty=2,add=TRUE)
+
+# Fit model
+source('~/Documents/git/SemiparametricRegression/probit.semipar.mixed.mcmc.R')
+start <- list(beta=beta,mu.beta=mu.beta,Lambda=Lambda,sigma.alpha=sigma.alpha,alpha=alpha)
+priors <- list(sigma.beta=10,S0=diag(qX),nu=qX+1,r=2,q=1)
+out1 <- probit.semipar.mixed.mcmc(y,X,g,W,priors,start,10000)
+
+# Examine estimates for beta_j and alpha_j
+g.idx <- 3  # group idx for plotting beta_j
+matplot(out1$beta[,,g.idx],type="l",lty=1);abline(h=beta[,g.idx],col=1:qX,lty=2)
+idx.tmp <- sample(1:qW[g.idx],3)
+matplot(out1$alpha[[g.idx]][,idx.tmp],type="l",lty=1)
+abline(h=alpha[[g.idx]][idx.tmp,],col=1:5,lty=2)
+
+matplot(out1$xi,type="l")
+
+# Compare to fixed effect model
+source('~/Documents/git/SemiparametricRegression/probit.semipar.mcmc.R', chdir = TRUE)
+start <- list(beta=c(beta[,g.idx]),alpha=c(alpha[[g.idx]]))
+# hist(sqrt(1/rgamma(1000,1,,2)),breaks=100)
+priors <- list(mu.beta=rep(0,qX),sigma.beta=10)
+out2 <- probit.semipar.mcmc(y[g==g.idx],X[g==g.idx,],W[[g.idx]],
+	priors=priors,start=start,sigma.alpha=sigma.alpha,n.mcmc=1000)
+matplot(out2$beta,type="l");abline(h=beta[,g.idx],col=1:qX)
+
+# Examine estimates for mu.beta
+matplot(out1$mu.beta,type="l");abline(h=mu.beta,col=1:qX,lty=2)
+
+# Examine estimates for Lambda
+matplot(cbind(out1$Lambda[1,1,],out1$Lambda[1,2,]),type="l")
+abline(h=c(Lambda[1,1],Lambda[1,2]),lty=2,col=1:qX)
+
+# Examine estimates for sigma.alpha
+matplot(out1$sigma.alpha,type="l")
+plot(apply(out1$sigma.alpha,1,mean),type="l")
+
+# Examine estimates for v
+boxplot(pnorm(out1$v),col=8,outline=FALSE)
+points(y,col=3)
+
